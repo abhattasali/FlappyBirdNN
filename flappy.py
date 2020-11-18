5c624b1f37bfa12f45db8e76df11a300b0303d64
Arun Bhattasali <abhattasali@gmail.com>
2020-11-17 18:16:58 -0800

    Wrote Bird Class and Added Movement

--- flappy.py
+++ flappy.py
@@ -1,3 +1,118 @@
 # Arun Bhattasali
 # Flappy Bird ML Generation
 # Fall 2020 - Tutorial: Python Flappy Bird AI (Tech with Tim)
+
+import pygame
+import neat
+import time
+import os
+import random
+
+# ------------ Constants ------------
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 800
+
+BIRD_IMAGES = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
+               pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
+               pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+
+PIPE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BACKGROUND_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+
+# ------------ Bird Class ------------
+class Bird:
+    IMAGES = BIRD_IMAGES
+    MAX_ROTATION = 25
+    ROT_VELOCITY = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.velocity = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMAGES[0]
+
+    def jump(self):
+        # 0,0 is top left of screen
+        self.velocity = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1
+        d = (self.velocity * self.tick_count) + 1.5 * self.tick_count ** 2
+
+        if d >= 16:
+            d = 16
+        if d < 0:
+            d -= 2
+        self.y += d
+
+        # Tilt (d < 0 means moving upwards) & (keep track of where we jump from, still tilt up if upwards momentum)
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VELOCITY
+
+    def draw(self, window):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMAGES[0]
+        elif self.img_count < self.ANIMATION_TIME * 2:
+            self.img = self.IMAGES[1]
+        elif self.img_count < self.ANIMATION_TIME * 3:
+            self.img = self.IMAGES[2]
+        elif self.img_count < self.ANIMATION_TIME * 4:
+            self.img = self.IMAGES[1]
+        elif self.img_count == self.ANIMATION_TIME * 4 + 1:
+            self.img = self.IMAGES[0]
+            self.img_count = 0
+
+        # When Nose-Dive, Do Not Flap
+        if self.tilt <= -80:
+            self.img = self.IMAGES[1]
+            self.img_count = self.ANIMATION_TIME * 2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
+        window.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+
+
+def draw_window(win, bird):
+    win.blit(BACKGROUND_IMAGE, (0, 0))
+    bird.draw(win)
+    pygame.display.update()
+
+
+def main():
+    bird = Bird(200, 200)
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # bird.move()
+        draw_window(window, bird)
+
+    pygame.quit()
+    quit()
+
+
+main()
